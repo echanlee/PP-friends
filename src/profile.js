e5e5f603ab3d6605b7c8a253ba291f785da3e9db
jj7lin <jj7lin@uwaterloo.ca>
2020-10-21 23:11:41 -0400

    test

--- src/profile.js
+++ src/profile.js
@@ -1,88 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-class MyForm extends React.Component {
+class MyProfileForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      age: null,
-      bio: '',
-      gender: 'Female'
+      username: "",
+      age: "null",
+      bio: "",
+      gender: "Female",
+      education: "",
+      interests: "",
     };
   }
 
+  handleUpdate = (event) => {
+    event.preventDefault();
+    alert("Your profile has been updated!");
+  };
 
-  myChangeHandler = (event) => {
-    console.log(event.target.files[0])
-    let nameField = event.target.name;
-    let fieldVal = event.target.fieldValue;
-    let errMsg = '';
-    if (nameField === "age") {
-      if (fieldVal != "" && !Number(fieldVal)) {
-        errMsg = <strong>infieldValid input, please enter a number</strong>;
-      }
-    }
-    this.setState({errormessage: errMsg});
-    this.setState({[nameField]: fieldVal});
-  }
-
-  handleUpdate = (event)=> {
-      event.preventDefault();
-      alert("Your profile has been updated!")
-  }
- 
   handleChange = (event) => {
-      this.setState({username: event.target.value});
-      this.setState({age: event.target.value});
-      this.setState({bio: event.target.value});
-      this.setState({gender: event.target.value});
-  }
+    this.setState({ username: event.target.value });
+    this.setState({ age: event.target.value });
+    this.setState({ bio: event.target.value });
+    this.setState({ gender: event.target.value });
+    this.setState({ education: event.target.value });
+    this.setState({ interests: event.target.value });
+  };
 
   render() {
     return (
       <form onUpdate={this.handleUpdate}>
-      <h1>My Profile</h1>
+        <h1>My Profile</h1>
 
-      <input type="file" name="file" onChange={this.myChangeHandler}/>
+        <input
+          type="file"
+          name="profile-pic-file"
+          onChange={this.myChangeHandler}
+        />
 
-      
-      <p>Username: </p>
+        <p>Username:</p>
 
-      <input type='text' name='username' onChange={this.myChangeHandler}/>
+        <input type="text" name="username" onChange={this.myChangeHandler} />
 
-      <p>Age:</p>
-      <input type='text' name='age' onChange={this.myChangeHandler}/>
+        <p>Age:</p>
 
-      <p>Your Gender:</p>
-      <select fieldValue = {this.state.gender} onChange ={this.myChangeHandler}>
-          <option fieldValue = "Female">Female</option>
-          <option fieldValue = "Male">Male</option>
-      </select>
+        <input
+          type="number"
+          name="age"
+          min="18"
+          max="100"
+          onChange={this.myChangeHandler}
+        />
 
-      <p>Your Preferred Gender for friends:</p>
-      <select fieldValue = {this.state.gender} onChange ={this.myChangeHandler}>
-          <option fieldValue = "Female">Female</option>
-          <option fieldValue = "Male">Male</option>
-      </select>
+        <p>Your Gender:</p>
 
-      <p>Education/Work:</p>
-      <input type='text' name='education' onChange={this.myChangeHandler}/>
+        <select fieldValue={this.state.gender} onChange={this.myChangeHandler}>
+          <option fieldValue="Female">Female</option>
+          <option fieldValue="Male">Male</option>
+          <option fieldValue="Other">Other</option>
+        </select>
 
-      <p>Your interests</p>
-      <input type='text' name='interests'onChange={this.myChangeHandler}/>
+        <p>Your Preferred Gender for friends:</p>
+        <select fieldValue={this.state.gender} onChange={this.myChangeHandler}>
+          <option fieldValue="Female">Female</option>
+          <option fieldValue="Male">Male</option>
+          <option fieldValue="Both">Both</option>
+        </select>
 
-       <p>Bio:</p>
-      <input type='text'name='bio' onChange={this.myChangeHandler} />
+        <p>Education/Work:</p>
+        <input type="text" name="education" onChange={this.myChangeHandler} />
 
-<button>Update!</button>
+        <p>Your interests:</p>
+        <input type="text" name="interests" onChange={this.myChangeHandler} />
 
-        </form>
+        <p>Bio:</p>
+        <input type="text" name="bio" onChange={this.myChangeHandler} />
+
+        <button>Update!</button>
+      </form>
     );
   }
 }
 
-ReactDOM.render(<MyForm />, document.getElementById('root'));
-
-
-    
+ReactDOM.render(<MyProfileForm />, document.getElementById("root"));
