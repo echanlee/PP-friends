a7afb994d0cdf40701b1d9345e9844232427e24b
Nadine Sison <mnsison@uwaterloo.ca>
2020-10-26 09:57:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ export default class App extends React.Component {
             <Router>
                 <div>
                     <Switch>
+                        <Route path="/questionnaire">
+                            <Questionnaire />
+                        </Route>
                         <Route path="/profile">
                             <ProfileForm />
                         </Route>
