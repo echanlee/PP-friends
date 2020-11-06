c27e8bfeb4b280862991b2a3ef616b6c356bc7a5
echanlee <40003439+echanlee@users.noreply.github.com>
2020-11-06 10:44:20 -0500

    Add messaging functionality (#15)
    * add messaging functionality
    
    * reformat and remove print
    
    * updates

--- src/Matches.js
+++ src/Matches.js
@@ -8,9 +8,42 @@ class Matches extends React.Component {
         this.state = {
             userId: this.props?.location?.state?.id,
             userIds: [],
-            matchesExist: "not set"
+            matchesExist: "not set",
+            firstnames: [],
+            name: "",
         }
+        this.selectUser = this.selectUser.bind(this);
     }
+    selectUser(event) {
+        const userSelected = event.target.value.split("|");
+        const myRequest = new Request('http://127.0.0.1:5000/conversationId', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "userId": this.state.userId,
+                "friendId": userSelected[0],
+            }),});
+        fetch(myRequest)
+            .then(response => response.json())
+            .then(res => {
+                this.props
+                .history.push({
+                  pathname: "/messages",
+                  state: {
+                      id: this.state.userId,
+                      friendId: userSelected[0],
+                      currentName: this.state.name,
+                      friendName: userSelected[1],
+                      currentConvoId: res.currentConvoId,
+                      friendConvoId: res.friendConvoId
+                }});
+            })
+        .catch((error) => {
+            alert("Something went wrong");
+            console.error(error)
+        });
+    }
+
     get_matches(){
         const myRequest = new Request('http://127.0.0.1:5000/matches', {
             method: 'POST',
@@ -24,8 +57,8 @@ class Matches extends React.Component {
                     this.setState({
                         matchesExist: "exists", 
                         userIds: res.userIds,
-                        conversationIds: res.conversationIds, 
-                        firstnames: res.firstnames
+                        firstnames: res.firstnames,
+                        name: res.currentName
                     })
                     :
                     this.setState({
@@ -49,6 +82,7 @@ class Matches extends React.Component {
         
     }
 
+
     render(){
         let matchingSection;
         if (this.state.matchesExist == "exists"){
@@ -56,10 +90,9 @@ class Matches extends React.Component {
             for (var i = 0; i < this.state.userIds.length; i++){
                 var pos_user = this.state.userIds[i];
                 userItems.push(
-                    <tr className='pos-user' key={pos_user}>
-                        {pos_user}
+                    <button className='pos-user' key={pos_user} value = {this.state.userIds[i]+"|"+this.state.firstnames[i]} onClick = {this.selectUser}>
                         {this.state.firstnames[i]}
-                    </tr>
+                    </button>
                 )
             }
             matchingSection = 
@@ -81,7 +114,7 @@ class Matches extends React.Component {
         return(
             <div id='Matches-section'>
                 <h2 id='Matches-header'>Matches</h2>
-                <div>{this.state.userId}</div>
+                <div>{this.state.name}</div>
                 <Link to={{pathname: '/main', state: {id: this.state.userId}}}>Back to Swiping!</Link>
                 <div id='Matches-section'>
                     {matchingSection}
---
+++ src/Messages.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import io from "socket.io-client";
+import {withRouter, Link} from 'react-router-dom'
+
+
+let endPoint = "http://localhost:5000";
+let socket = io.connect(`${endPoint}`);
+
+class Messages extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        userId: this.props?.location?.state?.id,
+        messages : [],
+        messageSender: [],
+        timeStamps: [],
+        message :"",
+        room: this.props?.location?.state?.friendConvoId,
+        currentName: this.props?.location?.state?.currentName,
+        friendName: this.props?.location?.state?.friendName,
+        friendId:  this.props?.location?.state?.friendId,
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  componentDidMount = () => {
+    const currentConvoId = this.props?.location?.state?.currentConvoId;
+    const myRequest = new Request('http://127.0.0.1:5000/getMessages', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({"convoId": currentConvoId,})
+            ,});
+        fetch(myRequest)
+            .then(response => response.json())
+            .then(res => {
+                this.setState({
+                    messages: res.messages,
+                    messageSender: res.fromNames,
+                    timeStamps: res.timeStamps,
+                  });
+
+                  socket.emit("room", currentConvoId);
+                  socket.on("message", msg => {
+                    this.setState({
+                        messages: [...this.state.messages, msg],
+                        messageSender: [...this.state.messageSender, this.state.friendName],
+                        timeStamps: [...this.state.timeStamps, new Date().toUTCString()],
+                      });
+                });
+            })
+        .catch((error) => {
+            console.error(error)
+        });
+  };
+
+
+  // On Change
+  onChange = e => {
+    this.setState({
+      message: e.target.value
+    });
+  };
+
+  // On Click
+  onClick = () => {
+    const message = this.state.message;
+    const name = this.state.currentName;
+    const room = this.state.room;
+    const currentConvoId = this.props?.location?.state?.currentConvoId;
+    if (message !== "") {
+      socket.emit("message", 
+        {
+          msg: (message),
+          room: room,
+        }
+      );
+      const myRequest = new Request('http://127.0.0.1:5000/sendMessage', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "convoId": currentConvoId,
+                "friendConvoId": this.state.room,
+                "currentId": this.state.userId,
+                "friendId": this.state.friendId,
+                "message": message
+            }),});
+        fetch(myRequest)
+            .then(response => response.json())
+            .then(res => {
+              console.log(res);
+                if(res.response != "Success")
+                    alert("Something went wrong sending message");
+            })
+        .catch((error) => {
+            console.error(error)
+        });
+      const date = new Date();
+      this.setState({
+        messages: [...this.state.messages, message],
+        messageSender: [...this.state.messageSender, name],
+        timeStamps: [...this.state.timeStamps, date.toUTCString()],
+        message: "",
+      });
+    } else {
+      alert("Please Add A Message");
+    }
+  };
+
+  onKeyPress = (e) => {
+    if(e.which === 13) {
+      this.onClick();
+    }
+  }
+
+  render() {
+    var message = this.state.message;
+    var messages = this.state.messages;
+    var timeStamps = this.state.timeStamps;
+    var names = this.state.messageSender;
+    return (
+      <div>
+        <Link to={{pathname: '/matches', state: {id: this.state.userId}}}>Back to Matches matches</Link>
+        {messages.length > 0 ?
+          messages.map((msg, index) => (
+            <div>
+              <p>{timeStamps[index]}</p>
+              <p>{names[index]} : {msg}</p>
+            </div>
+          )):
+          <header>Start a conversation!</header>}
+        <input value={message} name="message" onChange={e => this.onChange(e)} onKeyPress={this.onKeyPress} />
+        <button onClick={() =>this.onClick()} >Send Message</button> <br></br>
+      </div>
+    );
+    }
+}
+
+export default withRouter(Messages);
