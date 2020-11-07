82e52268c8f65294bd2d021c0d99c114f66a66c1
chalynpai <48136962+chalynpai@users.noreply.github.com>
2020-11-07 10:21:10 -0500

    Removed repitition of font and addressed comments

--- src/Login.js
+++ src/Login.js
@@ -61,7 +61,7 @@ class Login extends React.Component {
   render() {
     return (
       <div className = "Login" >
-        <div className = "top">
+        <div className = "logoBanner">
         <svg viewBox="0 0 500 200">
           <path id="curve" fill = "transparent" d="M73.2,200.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
           <text width="500">
