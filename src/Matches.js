f163e538150669dbb80c34affc9b1470f50349da
mtlssica <mtlssica1224@gmail.com>
2020-11-09 12:35:01 -0500

    test UI implementation with matching and without matching users

--- src/Matches.js
+++ src/Matches.js
@@ -119,12 +119,13 @@ class Matches extends React.Component {
         }
         return(
             <div id='Matches-section'>
-                 {matchingSection}
-             </div>                          
-                <div>{this.state.name}</div>
-                <Link to={{pathname: '/viewprofile', state: {id: this.state.userId}}}>View Profile</Link>
+                 {matchingSection}                        
+
                 <div class = "swipingButton" id = 'swipingButton'>
                     <Link to={{pathname: '/main', state: {id: this.state.userId}}}>Keep Swiping</Link>
+                </div>
+                <div class = "viewProfileButton" id = 'viewProfileButton'>
+                    <Link to={{pathname: '/viewprofile', state: {id: this.state.userId}}}>View Profile</Link>
                 </div>
             </div>
         )
