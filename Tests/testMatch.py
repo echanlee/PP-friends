be2d8841e8356d179fd8f7f02bf0b380502c6bf2
wclui <wclui@edu.uwaterloo.ca>
2020-11-09 01:02:09 -0500

    unit tests for back-end

---
+++ Tests/testMatch.py
@@ -0,0 +1,25 @@
+import unittest
+from server.profile import getProfile
+from server.matches import matchUser
+
+class TestMatch(unittest.TestCase):
+    def test_get_matched_user(self):
+        userId = 12
+        exp_d = {"response": "Success", "userIds": [11], "firstnames": ["melo2"], "currentName": "melo3"}
+        d = matchUser(userId)
+        self.assertEqual(exp_d, d)
+        
+    def test_get_matched_user_empty(self):
+        userId = 41
+        exp_d = {"response": "Success", "userIds": [], "firstnames": [], "currentName": "melo6"}
+        d = matchUser(userId)
+        self.assertEqual(exp_d, d)
+
+    def test_get_matched_user_multiple(self):
+        userId = 11
+        exp_d = {"response": "Success", "userIds": [12, 10], "firstnames": ["melo3","melo"], "currentName": "melo2"}
+        d = matchUser(userId)
+        self.assertEqual(exp_d, d)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Tests/testProfile.py
@@ -0,0 +1,22 @@
+import unittest
+from server.profile import getProfile
+from server.matches import matchUser
+
+class TestProfile(unittest.TestCase):
+    def test_get_valid_profile(self):
+        userId = 11
+        exp_profile = {"response": "Success", "name": 'melo2', "age": 21,\
+                 "bio": 'sad', "gender": 'Female', "education": 'loo',\
+                      "interests": 'sad', "genderPreference": 'Female', "maxDistance": 10
+                      }
+        profile = getProfile(userId)
+        self.assertEqual(exp_profile, profile)
+
+    def test_get_invalid_profile(self):
+        userId = -1
+        exp_profile = {"response": "invalid userId"}
+        profile = getProfile(userId)
+        self.assertEqual(exp_profile, profile)
+        
+if __name__ == '__main__':
+    unittest.main()
