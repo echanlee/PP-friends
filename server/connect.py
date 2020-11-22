81941eab478fc7254e4dc2c7b61175756ea68f7a
ca8marti-school <ca8marti@uwaterloo.ca>
2020-11-22 17:34:24 -0500

    added database connection

--- server/connect.py
+++ server/connect.py
@@ -6,6 +6,7 @@ config = {
     'user': 'root',
     'password': 'Msci342!',
     'host': '35.239.244.129',
+    'database': 'ppFriends',
     'client_flags': [ClientFlag.SSL],
     'ssl_ca': 'ssl/server-ca.pem',
     'ssl_cert': 'ssl/client-cert.pem',
