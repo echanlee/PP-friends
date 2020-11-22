579a42f5bdf139c8b46bacc24b9488da2e38a6f8
ca8marti-school <ca8marti@uwaterloo.ca>
2020-11-22 17:27:58 -0500

    added ssl certificates

--- server/connect.py
+++ server/connect.py
@@ -1,14 +1,20 @@
 import mysql.connector
 from mysql.connector import errorcode
+from mysql.connector.constants import ClientFlag
+
+config = {
+    'user': 'root',
+    'password': 'Msci342!',
+    'host': '35.239.244.129',
+    'client_flags': [ClientFlag.SSL],
+    'ssl_ca': 'ssl/server-ca.pem',
+    'ssl_cert': 'ssl/client-cert.pem',
+    'ssl_key': 'ssl/client-key.pem'
+}
 
 def connectToDB():
     try:
-        connection = mysql.connector.connect(
-            host="35.239.244.129",
-            database="ppFriends",
-            user="root",
-            password="Msci342!"
-        )
+        connection = mysql.connector.connect(**config)
         if connection.is_connected():
             return connection
         return False
