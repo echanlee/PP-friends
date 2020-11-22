87db31365521c384ce73a55b06eb4724cbeba600
ca8marti-school <ca8marti@uwaterloo.ca>
2020-11-22 12:27:51 -0500

    added 404 error handler

--- server/myapp.py
+++ server/myapp.py
@@ -29,9 +29,9 @@ def home():
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
 
-# @app.errorhandler(404)
-# def not_found(e):
-#     return app.send_static_file('index.js')
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.js')
 
 @app.route('/login', methods=['POST'])
 def login():
