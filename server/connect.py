92e56e49b1201671b829dde50345a26f9857d0ee
ca8marti-school <ca8marti@uwaterloo.ca>
2020-11-23 03:00:33 -0500

    ssl test

--- server/connect.py
+++ server/connect.py
@@ -1,16 +1,23 @@
 import mysql.connector
 from mysql.connector import errorcode
+from mysql.connector.constants import ClientFlag
+
+config = {
+    'user': 'root',
+    'password': 'Msci342!',
+    'host': '35.239.244.129',
+    'database': 'ppFriends',
+    'client_flags': [ClientFlag.SSL],
+    'ssl_ca': 'ssl/server-ca.pem',
+    'ssl_cert': 'ssl/client-cert.pem',
+    'ssl_key': 'ssl/client-key.pem'
+}
 
 def connectToDB():
     try:
-        connection = mysql.connector.connect(
-            host="35.239.244.129",
-            database="ppFriends",
-            user="root",
-            password="Msci342!"
-        )
+        connection = mysql.connector.connect(**config)
         if connection.is_connected():
             return connection
         return False
-    except mysql.connector.Error as e:
-        return e
+    except:
+        return False
