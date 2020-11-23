a642f0dde75abc2c1144349dccc49384c9806628
ca8marti-school <ca8marti@uwaterloo.ca>
2020-11-23 01:13:43 -0500

    test

--- server/myapp.py
+++ server/myapp.py
@@ -34,7 +34,7 @@ def login():
     print("test2")
     if request.method == 'POST':
     #   return loginUser(request.form['email'], request.form['password'])
-        return {"test": "test"}
+        return "testing"
 
 @app.route('/createprofile', methods=['POST'])
 def create_profile():
