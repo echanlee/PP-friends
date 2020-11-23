e2cace3b87c08f9465fdce31592d1489a9f2e9a9
ca8marti-school <ca8marti@uwaterloo.ca>
2020-11-23 12:07:53 -0500

    old database connection test

--- server/login.py
+++ server/login.py
@@ -5,7 +5,7 @@ from server.connect import connectToDB
 def loginUser(email, password):
     try:
         connection = connectToDB()
-        return {"connection": connection}
+        # return {"connection": connection}
         if(connection != False):
             cursor = connection.cursor(buffered=True)
 
