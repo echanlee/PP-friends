6ae8e2b7e88316f95029df17060aee455f97ae0d
ca8marti-school <ca8marti@uwaterloo.ca>
2020-11-23 13:22:16 -0500

    new production build

---
+++ build/precache-manifest.28761e37acaa9f5c10b83b7e9f8bd539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9794a18865450693fbe0da9c286e29a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d22c75ed74743f418e9e",
+    "url": "./static/css/main.6fedc70a.chunk.css"
+  },
+  {
+    "revision": "c0e75c849a7ae6310ab2",
+    "url": "./static/js/2.351b84ae.chunk.js"
+  },
+  {
+    "revision": "622c7b7cf2b666e809292bcaf29cf9ff",
+    "url": "./static/js/2.351b84ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22c75ed74743f418e9e",
+    "url": "./static/js/main.9755f4b7.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8ea24e5f261c5a18d366f83e5c40fbc1.js"
+  "./precache-manifest.28761e37acaa9f5c10b83b7e9f8bd539.js"
 );
 
 self.addEventListener('message', (event) => {
