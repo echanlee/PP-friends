cdf4af52e2c89715d380c13147513debf1e1ef03
ca8marti-school <ca8marti@uwaterloo.ca>
2020-11-23 01:20:10 -0500

    help

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c532191c5564c116b05f62c35783ac2c.js"
+  "./precache-manifest.4356d535cafa3cda72d37fdc7ad26fb6.js"
 );
 
 self.addEventListener('message', (event) => {
