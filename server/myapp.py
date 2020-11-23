51d806fea8bc871c9aa269a4877a61732c90db61
ca8marti-school <ca8marti@uwaterloo.ca>
2020-11-22 23:32:23 -0500

    updated static_url_path

--- server/myapp.py
+++ server/myapp.py
@@ -11,7 +11,7 @@ from server.questionnaire import updateQuestionnaire
 from server.potentialMatch import findPotentialMatches
 import server.messages
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='../build', static_url_path='')
 cors = CORS(app)
 
 app.config['SECRET_KEY'] = 'mysecret'
