f5d22803e7f45886c807f2eb981a47a3b700fed0
ca8marti-school <ca8marti@uwaterloo.ca>
2020-11-22 23:34:30 -0500

    added build files

---
+++ build/precache-manifest.0d0841ce895659dbf8717f03b9c13d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b61e848926225a860a642c5678d597e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "92515239b6d14ee2821a",
+    "url": "./static/css/main.3aeb517e.chunk.css"
+  },
+  {
+    "revision": "080d40ba704c3317ca38",
+    "url": "./static/js/2.9d37270f.chunk.js"
+  },
+  {
+    "revision": "622c7b7cf2b666e809292bcaf29cf9ff",
+    "url": "./static/js/2.9d37270f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92515239b6d14ee2821a",
+    "url": "./static/js/main.78c94227.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
