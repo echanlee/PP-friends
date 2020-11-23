e4084eb9d2f1b04c816a44c1c41ceb76842e14de
ca8marti-school <ca8marti@uwaterloo.ca>
2020-11-23 07:34:42 -0500

    Revert "connection test"
    This reverts commit cada33fa005068ec97792b9013891a12e947b9c5.

--- server/connect.py
+++ server/connect.py
@@ -10,8 +10,7 @@ config = {
     'client_flags': [ClientFlag.SSL],
     'ssl_ca': 'ssl/server-ca.pem',
     'ssl_cert': 'ssl/client-cert.pem',
-    'ssl_key': 'ssl/client-key.pem',
-    'ssl_verify_cert': 'True'
+    'ssl_key': 'ssl/client-key.pem'
 }
 
 def connectToDB():
