df25975d41c405c145a3675fef3a3ca5e053a448
Evette <evette.emcl@gmail.com>
2020-12-05 14:59:22 -0500

    improve query for performance and readibility

--- server/SwipeDecision.py
+++ server/SwipeDecision.py
@@ -85,27 +85,7 @@ def showProfile(currentUserId, shownUserId):
                 workPlace = row[5]
                 profile = row[6]
             
-            #uses set intersection to compare matches of each user to get a mutual friends list
-            currentUserMatchSet = getMatchIds(currentUserId, cursor, connection)
-            shownUserMatchSet = getMatchIds(shownUserId, cursor, connection)
-            mutualFriendIdList = currentUserMatchSet.intersection(shownUserMatchSet)
-
-            # if the user does not have a any mutual friends, don't run the name query, and return the request
-            if len(mutualFriendIdList) == 0:
-                connection.commit()
-                cursor.close()
-                return {"response": "Success", 
-                "firstName": firstName, 
-                "interests": interests, 
-                "description": description, 
-                "age": age, 
-                "gender": gender,
-                "workPlace": workPlace,
-                "profilePicture": profile,
-                "mutualFriendAmount": 0,
-                "mutualFriendNames": None}
-
-            mutualFriendNames = getMutualFriendNames(mutualFriendIdList, cursor, connection)
+            mutualFriendNames = getMutualFriendNames(currentUserId, shownUserId, cursor)
 
             connection.commit()
             cursor.close()
@@ -118,7 +98,7 @@ def showProfile(currentUserId, shownUserId):
             "gender": gender,
             "workPlace": workPlace,
             "profilePicture": profile,
-            "mutualFriendAmount": len(mutualFriendIdList),
+            "mutualFriendAmount": len(mutualFriendNames),
             "mutualFriendNames": mutualFriendNames}
     
     except mysql.connector.Error as err:
@@ -126,18 +106,12 @@ def showProfile(currentUserId, shownUserId):
 
     return{"response": "Something went wrong"}
 
-#gets all current matches that a user has
-def getMatchIds(id, cursor, connection):
-    sql = f"SELECT userTwo FROM Conversation WHERE userOne = {id};"
-    cursor.execute(sql)
-
-    idList = [ids[0] for ids in cursor.fetchall()]
-    return set(idList)
-
 # gets the names of friends to be outputted in the json response
-def getMutualFriendNames(mutualFriendsIdList, cursor, connection):
-    formattedList = ','.join(map(str, mutualFriendsIdList))
-    sql = f"SELECT firstname FROM Profile WHERE userId IN ({formattedList})"
+def getMutualFriendNames(currentUser, shownUser, cursor):
+    sql = f"SELECT p.firstname FROM Profile p where p.userId IN \
+            (SELECT userTwo FROM Conversation WHERE userOne = {currentUser} \
+                AND userTwo IN \
+                    (SELECT userTwo FROM Conversation WHERE userOne ={shownUser}))"
     cursor.execute(sql)
 
     mutualFriendNames = [names[0] for names in cursor.fetchall()]
